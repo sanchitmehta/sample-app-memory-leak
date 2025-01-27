9bae44107acd40a15959ba832bb24bab3540b922
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-26 18:47:23 -0800

    Fix memory leaks

--- Models/Requests.cs
+++ Models/Requests.cs
@@ -1,17 +1,92 @@
-﻿namespace PerformanceIssues.Models
+namespace PerformanceIssues.Models
 {
-    public class CacheEntryRequest
+    public class CacheEntryRequest : IDisposable
     {
         public int SizeMB { get; set; }
+
+        private bool _disposed;
+
+        ~CacheEntryRequest()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    // Clear managed resources if any
+                }
+                _disposed = true;
+            }
+        }
     }
 
-    public class CPUTaskRequest
+    public class CPUTaskRequest : IDisposable
     {
         public int Complexity { get; set; }
+
+        private bool _disposed;
+
+        ~CPUTaskRequest()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    // Clear managed resources if any
+                }
+                _disposed = true;
+            }
+        }
     }
 
-    public class DataGenerationRequest
+    public class DataGenerationRequest : IDisposable
     {
         public int RecordCount { get; set; }
+
+        private bool _disposed;
+
+        ~DataGenerationRequest()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    // Clear managed resources if any
+                }
+                _disposed = true;
+            }
+        }
     }
 }
