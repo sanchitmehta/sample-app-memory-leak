5a4c04e756189c9a7c5324bdbac960f61269d09b
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-26 18:47:29 -0800

    Fix memory leaks

--- Services/ICPUIntensiveTask.cs
+++ Services/ICPUIntensiveTask.cs
@@ -1,8 +1,62 @@
-﻿namespace PerformanceIssues.Serivces
+namespace PerformanceIssues.Services
 {
-    public interface ICPUIntensiveTask
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+
+    public interface ICPUIntensiveTask : IDisposable
     {
         void Start();
         void Stop();
+    }
+
+    public class CPUIntensiveTask : ICPUIntensiveTask
+    {
+        private readonly List<byte[]> _buffers = new List<byte[]>(); 
+        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+        private bool _disposed = false;
+
+        public void Start()
+        {
+            // Logic to start the task
+        }
+
+        public void Stop()
+        {
+            // Logic to stop the task, including canceling the token
+            _cancellationTokenSource.Cancel();
+        }
+
+        private void ClearResources()
+        {
+            // Clear large collections and other potential memory leaks
+            _buffers.Clear();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    // Dispose managed resources
+                    ClearResources();
+                    _cancellationTokenSource.Dispose();
+                }
+
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~CPUIntensiveTask()
+        {
+            Dispose(false);
+        }
     }
 }
