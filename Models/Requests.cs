bbcbea9187740003f9b4120e2224c1c2fae6a294
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-27 08:21:37 -0800

    Fix memory leaks in System.Collections.ObjectModel.ReadOnlyCollection`1[Octokit.RepositoryContent]

--- Models/Requests.cs
+++ Models/Requests.cs
@@ -1,17 +1,106 @@
-﻿namespace PerformanceIssues.Models
+using System;
+
+namespace PerformanceIssues.Models
 {
-    public class CacheEntryRequest
+    public class CacheEntryRequest : IDisposable
     {
         public int SizeMB { get; set; }
+        private bool disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // Dispose managed state (managed objects)
+                    // No managed objects to dispose in this class
+                }
+                // Free unmanaged resources (unmanaged objects) and override finalizer
+                // Set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // Override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        ~CacheEntryRequest()
+        {
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
     }
 
-    public class CPUTaskRequest
+    public class CPUTaskRequest : IDisposable
     {
         public int Complexity { get; set; }
+        private bool disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // Dispose managed state (managed objects)
+                    // No managed objects to dispose in this class
+                }
+                // Free unmanaged resources (unmanaged objects) and override finalizer
+                // Set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // Override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        ~CPUTaskRequest()
+        {
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
     }
 
-    public class DataGenerationRequest
+    public class DataGenerationRequest : IDisposable
     {
         public int RecordCount { get; set; }
+        private bool disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // Dispose managed state (managed objects)
+                    // No managed objects to dispose in this class
+                }
+                // Free unmanaged resources (unmanaged objects) and override finalizer
+                // Set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // Override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        ~DataGenerationRequest()
+        {
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
