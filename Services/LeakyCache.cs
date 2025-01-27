8007cb78554cec15d2a32511dcbfdb90377bba6e
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-27 11:19:36 -0800

    Fix memory leaks in System.Collections.ObjectModel.ReadOnlyCollection`1[Octokit.RepositoryContent]

--- Services/LeakyCache.cs
+++ Services/LeakyCache.cs
@@ -1,14 +1,18 @@
-﻿using System.Collections.Concurrent;
+using System.Collections.Concurrent;
 
-namespace PerformanceIssues.Serivces
+namespace PerformanceIssues.Services
 {
-    public class LeakyCache : ILeakyCache
+    public class LeakyCache : ILeakyCache, IDisposable
     {
         private static readonly ConcurrentDictionary<string, byte[]> _cache = new();
-        private static readonly Random _random = new();
+        private readonly Random _random = new();
+        private bool _disposed = false;
 
         public async Task<string> AddToCache(string key, int sizeInMb)
         {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(LeakyCache));
+            
             // Intentionally creating large byte arrays and storing them indefinitely
             byte[] data = new byte[sizeInMb * 1024 * 1024];
             _random.NextBytes(data);
@@ -17,9 +21,40 @@ namespace PerformanceIssues.Serivces
             await Task.Delay(100);
 
             _cache.TryAdd(key, data);
+
             return key;
         }
 
-        public int GetCacheSize() => _cache.Count;
+        public int GetCacheSize()
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(LeakyCache));
+                
+            return _cache.Count;
+        }
+
+        public void ClearCache()
+        {
+            _cache.Clear();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    ClearCache();
+                }
+
+                _disposed = true;
+            }
+        }
     }
 }
