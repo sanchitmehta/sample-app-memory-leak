91c143f85a0dd75be4855a9ea230f57ac0f8b076
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-26 21:25:12 -0800

    Fix memory leaks in System.Collections.ObjectModel.ReadOnlyCollection`1[Octokit.RepositoryContent]

--- Services/ICPUIntensiveTask.cs
+++ Services/ICPUIntensiveTask.cs
@@ -1,8 +1,87 @@
-﻿namespace PerformanceIssues.Serivces
+namespace PerformanceIssues.Services
 {
-    public interface ICPUIntensiveTask
+    using System;
+    using System.Threading;
+    using System.Collections.Generic;
+
+    public interface ICPUIntensiveTask : IDisposable
     {
         void Start();
         void Stop();
+    }
+
+    public class CPUIntensiveTask : ICPUIntensiveTask
+    {
+        private CancellationTokenSource _cancellationTokenSource;
+        private bool _disposed;
+        private List<byte[]> _largeByteArrayCache;
+
+        public CPUIntensiveTask()
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+            _largeByteArrayCache = new List<byte[]>();
+        }
+
+        public void Start()
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(CPUIntensiveTask));
+
+            // Start logic for CPU-intensive task
+        }
+
+        public void Stop()
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(CPUIntensiveTask));
+
+            _cancellationTokenSource?.Cancel();
+
+            ClearLargeCollections();
+        }
+
+        private void ClearLargeCollections()
+        {
+            if (_largeByteArrayCache != null)
+            {
+                _largeByteArrayCache.Clear();
+                _largeByteArrayCache = null;
+            }
+        }
+
+        private void ClearEventHandlers()
+        {
+            // Assume there are events, clearing them here
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    ClearEventHandlers();
+
+                    // Dispose managed resources
+                    _cancellationTokenSource?.Dispose();
+                    ClearLargeCollections();
+                }
+
+                // Free unmanaged resources if any
+
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~CPUIntensiveTask()
+        {
+            Dispose(disposing: false);
+        }
     }
 }
