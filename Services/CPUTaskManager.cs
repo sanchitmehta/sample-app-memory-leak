f9c27970489bdce87ab2a9af0018b789aeb8bf4b
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-26 21:25:09 -0800

    Fix memory leaks in System.Collections.ObjectModel.ReadOnlyCollection`1[Octokit.RepositoryContent]

--- Services/CPUTaskManager.cs
+++ Services/CPUTaskManager.cs
@@ -1,25 +1,48 @@
-﻿using System.Collections.Concurrent;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 
-namespace PerformanceIssues.Serivces
+namespace PerformanceIssues.Services
 {
-    public class CPUTaskManager
+    public class CPUTaskManager : IDisposable
     {
         private readonly ConcurrentDictionary<string, ICPUIntensiveTask> _activeTasks = new();
+        private bool _disposed;
 
         public string StartNewTask(int complexity)
         {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(CPUTaskManager));
+
             var taskId = Guid.NewGuid().ToString();
             var task = new CPUIntensiveTask(complexity);
-            task.Start();
-            _activeTasks.TryAdd(taskId, task);
+            
+            try
+            {
+                task.Start();
+                if (!_activeTasks.TryAdd(taskId, task))
+                {
+                    task.Stop(); 
+                }
+            }
+            catch
+            {
+                task.Dispose(); 
+                throw;
+            }
+            
             return taskId;
         }
 
         public bool StopTask(string taskId)
         {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(CPUTaskManager));
+
             if (_activeTasks.TryRemove(taskId, out var task))
             {
                 task.Stop();
+                task.Dispose();
                 return true;
             }
             return false;
@@ -27,16 +50,93 @@ namespace PerformanceIssues.Serivces
 
         public void StopAllTasks()
         {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(CPUTaskManager));
+
             foreach (var task in _activeTasks.Values)
             {
                 task.Stop();
+                task.Dispose();
             }
             _activeTasks.Clear();
         }
 
         public IEnumerable<string> GetActiveTasks()
         {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(CPUTaskManager));
+
             return _activeTasks.Keys;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    StopAllTasks();
+                }
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    public interface ICPUIntensiveTask : IDisposable
+    {
+        void Start();
+        void Stop();
+    }
+
+    public class CPUIntensiveTask : ICPUIntensiveTask
+    {
+        private readonly int _complexity;
+        private bool _disposed;
+
+        public CPUIntensiveTask(int complexity)
+        {
+            _complexity = complexity;
+        }
+
+        public void Start()
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(CPUIntensiveTask));
+
+            // Start task logic here
+        }
+
+        public void Stop()
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(CPUIntensiveTask));
+
+            // Stop task logic here
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    // Release managed resources here
+                }
+                // Release unmanaged resources here
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
     }
 }
