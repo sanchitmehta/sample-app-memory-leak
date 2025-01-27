46a35c7f8ab6595b7e71ed0eac33ff51169388d6
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-26 21:57:09 -0800

    Fix memory leaks in System.Collections.ObjectModel.ReadOnlyCollection`1[Octokit.RepositoryContent]

--- Services/IEventManager.cs
+++ Services/IEventManager.cs
@@ -1,8 +1,72 @@
-﻿namespace PerformanceIssues.Serivces
+namespace PerformanceIssues.Services
 {
-    public interface IEventManager
+    public interface IEventManager : IDisposable
     {
         void Subscribe(Action<string> handler);
         void RaiseEvent(string message);
+    }
+}
+
+public class EventManager : IEventManager
+{
+    private event Action<string> _event;
+    private bool _disposed = false;
+    
+    public void Subscribe(Action<string> handler)
+    {
+        _event += handler;
+    }
+
+    public void RaiseEvent(string message)
+    {
+        _event?.Invoke(message);
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed)
+        {
+            if (disposing)
+            {
+                // Clear event handlers
+                if (_event != null)
+                {
+                    foreach (Delegate d in _event.GetInvocationList())
+                    {
+                        _event -= (Action<string>)d;
+                    }
+                }
+            }
+            _disposed = true;
+        }
+    }
+
+    ~EventManager()
+    {
+        Dispose(false);
+    }
+}
+
+public class EventManagerUsage
+{
+    public void UseEventManager()
+    {
+        using (var eventManager = new EventManager())
+        {
+            eventManager.Subscribe(OnEventRaised);
+            eventManager.RaiseEvent("Test event");
+        }
+        // eventManager is disposed here automatically
+    }
+
+    private void OnEventRaised(string message)
+    {
+        Console.WriteLine(message);
     }
 }
