1ecda89df21feeed0f442f32be0358ec55961a4a
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-27 11:19:32 -0800

    Fix memory leaks in System.Collections.ObjectModel.ReadOnlyCollection`1[Octokit.RepositoryContent]

--- Services/DataGenerator.cs
+++ Services/DataGenerator.cs
@@ -1,9 +1,10 @@
 ﻿namespace PerformanceIssues.Services
 {
-    public class DataGenerator
+    public class DataGenerator : IDisposable
     {
         private readonly List<object> _storedData = new();
         private readonly Random _random = new();
+        private bool _disposed = false;
 
         public async Task GenerateAndStoreData(int count)
         {
@@ -33,6 +34,28 @@
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
             return new string(Enumerable.Repeat(chars, length)
                 .Select(s => s[_random.Next(s.Length)]).ToArray());
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                // Clean up managed resources
+                _storedData.Clear();
+            }
+
+            // Clean up unmanaged resources
+
+            _disposed = true;
         }
     }
 }
