882b991c8a7272a3d4f044a30fd9681dee3978c6
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-27 10:34:11 -0800

    Fix memory leaks in System.Collections.ObjectModel.ReadOnlyCollection`1[Octokit.RepositoryContent]

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
-using PerformanceIssues.Serivces;
 using PerformanceIssues.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
-
 
 builder.Services.AddControllers();
 // builder.Services.AddOpenApi();
@@ -12,6 +12,17 @@ builder.Services.AddSingleton<ILeakyCache, LeakyCache>();
 builder.Services.AddSingleton<IEventManager, EventManager>();
 builder.Services.AddSingleton<DataGenerator>();
 builder.Services.AddSingleton<CPUTaskManager>();
+builder.Services.AddSingleton<ILoggerFactory>(provider =>
+{
+    var factory = LoggerFactory.Create(loggingBuilder =>
+    {
+        loggingBuilder.SetMinimumLevel(LogLevel.Debug);
+        loggingBuilder.AddConsole();
+        loggingBuilder.AddDebug();
+    });
+
+    return factory;
+});
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
@@ -24,5 +35,21 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.MapControllers();
+
+app.Lifetime.ApplicationStopping.Register(() =>
+{
+    var leakyCache = app.Services.GetRequiredService<ILeakyCache>() as IDisposable;
+    var eventManager = app.Services.GetRequiredService<IEventManager>() as IDisposable;
+    var dataGenerator = app.Services.GetRequiredService<DataGenerator>() as IDisposable;
+    var cpuTaskManager = app.Services.GetRequiredService<CPUTaskManager>() as IDisposable;
+
+    leakyCache?.Dispose();
+    eventManager?.Dispose();
+    dataGenerator?.Dispose();
+    cpuTaskManager?.Dispose();
+    
+    var loggerFactory = app.Services.GetRequiredService<ILoggerFactory>();
+    loggerFactory.Dispose();
+});
 
 app.Run();
