020c20ba00449fc4232a49cd0fa39439ef47042b
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-26 18:47:27 -0800

    Fix memory leaks

--- Services/DataGenerator.cs
+++ Services/DataGenerator.cs
@@ -1,14 +1,24 @@
-﻿namespace PerformanceIssues.Services
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PerformanceIssues.Services
 {
-    public class DataGenerator
+    public class DataGenerator : IDisposable
     {
         private readonly List<object> _storedData = new();
         private readonly Random _random = new();
+        private bool _disposed;
 
-        public async Task GenerateAndStoreData(int count)
+        public async Task GenerateAndStoreData(int count, CancellationToken cancellationToken)
         {
             for (int i = 0; i < count; i++)
             {
+                cancellationToken.ThrowIfCancellationRequested();
+
                 var data = new
                 {
                     Id = Guid.NewGuid(),
@@ -19,13 +29,18 @@
                 };
 
                 _random.NextBytes(data.Data);
-                _storedData.Add(data);  // Memory leak: storing without bounds
+                _storedData.Add(data);
 
                 if (i % 1000 == 0)
                 {
-                    await Task.Delay(1);  // Give other threads a chance to run
+                    await Task.Delay(1, cancellationToken);
                 }
             }
+        }
+
+        public void ClearStoredData()
+        {
+            _storedData.Clear();
         }
 
         private string GenerateRandomString(int length)
@@ -33,6 +48,25 @@
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
             return new string(Enumerable.Repeat(chars, length)
                 .Select(s => s[_random.Next(s.Length)]).ToArray());
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _storedData.Clear();
+                }
+
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
     }
 }
