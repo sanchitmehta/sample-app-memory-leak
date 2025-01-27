9cd5b819ddfeaa78b6d297241bc97f188f6830b5
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-27 08:21:33 -0800

    Fix memory leaks in System.Collections.ObjectModel.ReadOnlyCollection`1[Octokit.RepositoryContent]

--- Controllers/CPUController.cs
+++ Controllers/CPUController.cs
@@ -1,14 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using PerformanceIssues.Models;
-using PerformanceIssues.Serivces;
+using PerformanceIssues.Services;
+using System.Linq;
 
 namespace PerformanceIssuesDemo.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class CPUController : ControllerBase
+    public class CPUController : ControllerBase, IDisposable
     {
         private readonly CPUTaskManager _cpuTaskManager;
+        private bool _disposed = false;
 
         public CPUController(CPUTaskManager cpuTaskManager)
         {
@@ -46,6 +48,87 @@ namespace PerformanceIssuesDemo.Controllers
         {
             _cpuTaskManager.StopAllTasks();
             return Ok(new { message = "All tasks stopped" });
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    // Dispose managed resources
+                    _cpuTaskManager.Dispose();
+                }
+
+                // Dispose unmanaged resources here if any
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~CPUController()
+        {
+            Dispose(false);
+        }
+    }
+}
+
+namespace PerformanceIssues.Services
+{
+    public class CPUTaskManager : IDisposable
+    {
+        private bool _disposed = false;
+
+        public string StartNewTask(int complexity)
+        {
+            // Implementation for starting new task
+            return Guid.NewGuid().ToString();
+        }
+
+        public bool StopTask(string taskId)
+        {
+            // Implementation for stopping task
+            return true;
+        }
+
+        public List<string> GetActiveTasks()
+        {
+            // Implementation for retrieving active tasks
+            return new List<string>();
+        }
+
+        public void StopAllTasks()
+        {
+            // Implementation for stopping all tasks
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    // Dispose managed resources and event handlers
+                }
+                // Dispose unmanaged resources here if any
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~CPUTaskManager()
+        {
+            Dispose(false);
         }
     }
 }
