a043561c8e3c5f75d95b5b4ec86d221e6318bfc8
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-26 18:47:31 -0800

    Fix memory leaks

--- Services/LeakyCache.cs
+++ Services/LeakyCache.cs
@@ -1,14 +1,18 @@
-﻿using System.Collections.Concurrent;
+using System;
+using System.Collections.Concurrent;
 
-namespace PerformanceIssues.Serivces
+namespace PerformanceIssues.Services
 {
-    public class LeakyCache : ILeakyCache
+    public class LeakyCache : ILeakyCache, IDisposable
     {
-        private static readonly ConcurrentDictionary<string, byte[]> _cache = new();
-        private static readonly Random _random = new();
+        private readonly ConcurrentDictionary<string, byte[]> _cache = new();
+        private readonly Random _random = new();
+        private bool _disposed;
 
         public async Task<string> AddToCache(string key, int sizeInMb)
         {
+            if (_disposed) throw new ObjectDisposedException(nameof(LeakyCache));
+
             // Intentionally creating large byte arrays and storing them indefinitely
             byte[] data = new byte[sizeInMb * 1024 * 1024];
             _random.NextBytes(data);
@@ -20,6 +24,37 @@ namespace PerformanceIssues.Serivces
             return key;
         }
 
-        public int GetCacheSize() => _cache.Count;
+        public int GetCacheSize()
+        {
+            if (_disposed) throw new ObjectDisposedException(nameof(LeakyCache));
+            return _cache.Count;
+        }
+
+        public void ClearCache()
+        {
+            if (_disposed) throw new ObjectDisposedException(nameof(LeakyCache));
+            foreach (var key in _cache.Keys)
+            {
+                _cache.TryRemove(key, out _);
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    ClearCache();
+                }
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
