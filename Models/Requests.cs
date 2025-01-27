6df304cc4f7b6e71ff55e5fbd8822579fd87510d
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-26 21:57:03 -0800

    Fix memory leaks in System.Collections.ObjectModel.ReadOnlyCollection`1[Octokit.RepositoryContent]

--- Models/Requests.cs
+++ Models/Requests.cs
@@ -1,17 +1,104 @@
-﻿namespace PerformanceIssues.Models
+namespace PerformanceIssues.Models
 {
-    public class CacheEntryRequest
+    public class CacheEntryRequest : IDisposable
     {
         public int SizeMB { get; set; }
+
+        // Dispose pattern
+        private bool disposed = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    // Free other state (managed objects).
+                }
+                // Free your own state (unmanaged objects).
+                // Set large fields to null.
+                disposed = true;
+            }
+        }
+
+        ~CacheEntryRequest()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(false);
+        }
     }
 
-    public class CPUTaskRequest
+    public class CPUTaskRequest : IDisposable
     {
         public int Complexity { get; set; }
+
+        // Dispose pattern
+        private bool disposed = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    // Free other state (managed objects).
+                }
+                // Free your own state (unmanaged objects).
+                // Set large fields to null.
+                disposed = true;
+            }
+        }
+
+        ~CPUTaskRequest()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(false);
+        }
     }
 
-    public class DataGenerationRequest
+    public class DataGenerationRequest : IDisposable
     {
         public int RecordCount { get; set; }
+
+        // Dispose pattern
+        private bool disposed = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    // Free other state (managed objects).
+                }
+                // Free your own state (unmanaged objects).
+                // Set large fields to null.
+                disposed = true;
+            }
+        }
+
+        ~DataGenerationRequest()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(false);
+        }
     }
 }
