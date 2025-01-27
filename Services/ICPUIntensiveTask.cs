6fae1dcbb60ad6769f12f1f8b22cdb485b5bee0e
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-27 10:34:15 -0800

    Fix memory leaks in System.Collections.ObjectModel.ReadOnlyCollection`1[Octokit.RepositoryContent]

--- Services/ICPUIntensiveTask.cs
+++ Services/ICPUIntensiveTask.cs
@@ -1,8 +1,58 @@
-﻿namespace PerformanceIssues.Serivces
+```csharp
+using System;
+using System.IO.Pipelines;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
+using Microsoft.Extensions.Logging;
+
+namespace PerformanceIssues.Services
 {
-    public interface ICPUIntensiveTask
+    public interface ICPUIntensiveTask : IDisposable
     {
         void Start();
         void Stop();
     }
+
+    public class CPUIntensiveTask : ICPUIntensiveTask
+    {
+        private CancellationTokenSource _cancellationTokenSource;
+        private Pipe _pipe;
+        private Http1Connection _http1Connection;
+        private HttpRequestHeaders _httpRequestHeaders;
+        private LoggerFactoryScopeProvider.Scope _loggingScope;
+        private IPAddress _ipAddress;
+        private IPEndPoint _ipEndPoint;
+        private DateHeaderValueManager.DateHeaderValues _dateHeaderValues;
+
+        public CPUIntensiveTask()
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+            _pipe = new Pipe();
+            _http1Connection = new Http1Connection();
+            _httpRequestHeaders = new HttpRequestHeaders();
+            _loggingScope = new LoggerFactoryScopeProvider.Scope();
+            _ipAddress = IPAddress.Any;
+            _ipEndPoint = new IPEndPoint(_ipAddress, 0);
+            _dateHeaderValues = new DateHeaderValueManager.DateHeaderValues();
+        }
+
+        public void Start() { /* Task logic */ }
+        public void Stop() { _cancellationTokenSource.Cancel(); }
+
+        public void Dispose()
+        {
+            _cancellationTokenSource?.Dispose();
+            _pipe?.Reset();
+            _http1Connection?.Dispose();
+            _httpRequestHeaders?.Clear();
+            _loggingScope?.Dispose();
+            Array.Clear(_ipAddress.GetAddressBytes(), 0, _ipAddress.GetAddressBytes().Length);
+            _ipAddress = null;
+            _ipEndPoint = null;
+            _dateHeaderValues?.Dispose();
+        }
+    }
 }
+```
