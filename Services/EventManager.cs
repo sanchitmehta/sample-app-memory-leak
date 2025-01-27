d60eadd77ae4ed85bfbded685bad6981d2d57d4c
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-26 18:47:28 -0800

    Fix memory leaks

--- Services/EventManager.cs
+++ Services/EventManager.cs
@@ -1,31 +1,68 @@
-﻿namespace PerformanceIssues.Serivces
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PerformanceIssues.Services
 {
-    public class EventManager : IEventManager
+    public class EventManager : IEventManager, IDisposable
     {
         private readonly List<WeakReference> _subscribers = new();
-        private readonly List<Action<string>> _strongSubscribers = new();  // Intentional memory leak
+        private readonly List<Action<string>> _strongSubscribers = new();
+        private bool _disposed;
 
         public void Subscribe(Action<string> handler)
         {
-            // Memory leak: storing both weak and strong references
+            if (handler == null)
+                throw new ArgumentNullException(nameof(handler));
+
             _subscribers.Add(new WeakReference(handler));
-            _strongSubscribers.Add(handler);  // This prevents garbage collection
         }
 
         public void RaiseEvent(string message)
         {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(EventManager));
+
             foreach (var weakRef in _subscribers.ToList())
             {
                 if (weakRef.Target is Action<string> handler)
                 {
                     handler(message);
                 }
+                else
+                {
+                    _subscribers.Remove(weakRef);
+                }
             }
+        }
 
-            foreach (var handler in _strongSubscribers)
+        public void ClearSubscribers()
+        {
+            _subscribers.Clear();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
             {
-                handler(message);
+                if (disposing)
+                {
+                    _subscribers.Clear();
+                    _strongSubscribers.Clear();
+                }
+                _disposed = true;
             }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~EventManager()
+        {
+            Dispose(false);
         }
     }
 }
