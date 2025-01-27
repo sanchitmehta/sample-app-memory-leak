e33e63826386d30e73992fe177424269aeab0bd4
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-27 08:21:49 -0800

    Fix memory leaks in System.Collections.ObjectModel.ReadOnlyCollection`1[Octokit.RepositoryContent]

--- Services/ILeakyCache.cs
+++ Services/ILeakyCache.cs
@@ -1,8 +1,35 @@
-﻿namespace PerformanceIssues.Serivces
+namespace PerformanceIssues.Services
 {
-    public interface ILeakyCache
+    public interface ILeakyCache : IDisposable
     {
         Task<string> AddToCache(string key, int sizeInMb);
         int GetCacheSize();
+    }
+    
+    public class LeakyCache : ILeakyCache
+    {
+        private readonly Dictionary<string, byte[]> _cache = new Dictionary<string, byte[]>();
+
+        public Task<string> AddToCache(string key, int sizeInMb)
+        {
+            byte[] data = new byte[sizeInMb * 1024 * 1024];
+            _cache[key] = data;
+            return Task.FromResult(key);
+        }
+
+        public int GetCacheSize()
+        {
+            return _cache.Values.Sum(arr => arr.Length) / (1024 * 1024);
+        }
+
+        public void Dispose()
+        {
+            ClearCache();
+        }
+
+        private void ClearCache()
+        {
+            _cache.Clear();
+        }
     }
 }
