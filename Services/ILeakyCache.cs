cf793124e30061149426881f5d7686ad98b9f9b5
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-28 13:19:36 -0800

    Fix memory leaks in System.Collections.ObjectModel.ReadOnlyCollection`1[Octokit.RepositoryContent]

--- Services/ILeakyCache.cs
+++ Services/ILeakyCache.cs
@@ -1,8 +1,83 @@
-﻿namespace PerformanceIssues.Serivces
+namespace PerformanceIssues.Services
 {
-    public interface ILeakyCache
+    public interface ILeakyCache : IDisposable
     {
         Task<string> AddToCache(string key, int sizeInMb);
         int GetCacheSize();
+    }
+
+    public class LeakyCache : ILeakyCache
+    {
+        private readonly Dictionary<string, byte[]> _cache = new Dictionary<string, byte[]>();
+        private readonly List<WeakReference> _resources = new List<WeakReference>();
+        private bool _disposed = false;
+
+        public async Task<string> AddToCache(string key, int sizeInMb)
+        {
+            var memoryStream = new MemoryStream();
+            try
+            {
+                byte[] data = new byte[sizeInMb * 1024 * 1024];
+                var random = new Random();
+                random.NextBytes(data);
+
+                memoryStream.Write(data, 0, data.Length);
+                _cache[key] = data;
+                _resources.Add(new WeakReference(memoryStream));
+
+                return await Task.FromResult(key);
+            }
+            finally
+            {
+                memoryStream.Dispose();
+            }
+        }
+
+        public int GetCacheSize()
+        {
+            int size = 0;
+            foreach (var kvp in _cache)
+            {
+                size += kvp.Value.Length;
+            }
+            return size;
+        }
+
+        public void ClearCache()
+        {
+            _cache.Clear();
+            _resources.Clear();
+            GC.Collect();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    foreach (var weakRef in _resources)
+                    {
+                        if (weakRef.IsAlive)
+                        {
+                            (weakRef.Target as IDisposable)?.Dispose();
+                        }
+                    }
+                    _resources.Clear();
+                }
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~LeakyCache()
+        {
+            Dispose(false);
+        }
     }
 }
