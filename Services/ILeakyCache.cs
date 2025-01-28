4dae85e91f2f02c319b21b623a6291b1077f664f
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-27 18:41:37 -0800

    Fix memory leaks in System.Collections.ObjectModel.ReadOnlyCollection`1[Octokit.RepositoryContent]

--- Services/ILeakyCache.cs
+++ Services/ILeakyCache.cs
@@ -1,8 +1,61 @@
-﻿namespace PerformanceIssues.Serivces
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PerformanceIssues.Services
 {
-    public interface ILeakyCache
+    public interface ILeakyCache : IDisposable
     {
         Task<string> AddToCache(string key, int sizeInMb);
         int GetCacheSize();
+    }
+
+    public sealed class LeakyCache : ILeakyCache
+    {
+        private readonly Dictionary<string, byte[]> _cache = new Dictionary<string, byte[]>();
+        private bool _disposed = false;
+
+        public async Task<string> AddToCache(string key, int sizeInMb)
+        {
+            if (_disposed) throw new ObjectDisposedException(nameof(LeakyCache));
+
+            byte[] data = new byte[sizeInMb * 1024 * 1024];
+            using (var ms = new MemoryStream(data))
+            {
+                await ms.WriteAsync(data, 0, data.Length).ConfigureAwait(false);
+                _cache[key] = data;
+            }
+
+            return key;
+        }
+
+        public int GetCacheSize()
+        {
+            if (_disposed) throw new ObjectDisposedException(nameof(LeakyCache));
+
+            return _cache.Values.Sum(v => v.Length);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+
+            if (disposing)
+            {
+                _cache.Clear();
+            }
+
+            _disposed = true;
+        }
+
+        ~LeakyCache()
+        {
+            Dispose(false);
+        }
     }
 }
