79c5a7e64b825d2575d303906190874565da54bd
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-27 18:41:30 -0800

    Fix memory leaks in System.Collections.ObjectModel.ReadOnlyCollection`1[Octokit.RepositoryContent]

--- Controllers/MemoryController.cs
+++ Controllers/MemoryController.cs
@@ -1,8 +1,8 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using PerformanceIssues.Models;
-using PerformanceIssues.Serivces;
 using PerformanceIssues.Services;
+using PerformanceIssues.Serivces;
 
 namespace PerformanceIssuesDemo.Controllers
 {
@@ -44,9 +44,21 @@ namespace PerformanceIssuesDemo.Controllers
         public async Task<IActionResult> Subscribe()
         {
             var id = Guid.NewGuid().ToString();
-            Action<string> handler = msg => Console.WriteLine($"Event received for {id}: {msg}");
+            Action<string> handler = msg =>
+            {
+                Console.WriteLine($"Event received for {id}: {msg}");
+            };
             _eventManager.Subscribe(handler);
-            await Task.Run(() => _eventManager.RaiseEvent($"Test event for {id}"));
+
+            try
+            {
+                await Task.Run(() => _eventManager.RaiseEvent($"Test event for {id}"));
+            }
+            finally
+            {
+                _eventManager.Unsubscribe(handler);
+            }
+
             return Ok(new { subscriberId = id });
         }
 
@@ -71,28 +83,28 @@ namespace PerformanceIssuesDemo.Controllers
                 RedirectStandardError = true,
                 UseShellExecute = false
             };
-    
+
             using var process = Process.Start(processStartInfo);
             if (process is null)
             {
                 return BadRequest("Failed to start memory dump process");
             }
-    
+
             var output = process.StandardOutput.ReadToEnd();
             var error = process.StandardError.ReadToEnd();
             process.WaitForExit();
-    
+
             if (process.ExitCode != 0)
             {
                 return BadRequest(new { error });
             }
-    
+
             // Parse and limit to top 100 objects
             var lines = output.Split('\n')
                 .Where(l => !string.IsNullOrWhiteSpace(l))
                 .Where(l => l.Contains("   ")) // Filter memory dump lines
                 .Take(100);
-    
+
             return Ok(new { memoryDump = string.Join("\n", lines) });
         }
     }
