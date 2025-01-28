257a59ca7b331aeffb5ed662948011d03481a7a5
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-27 18:41:36 -0800

    Fix memory leaks in System.Collections.ObjectModel.ReadOnlyCollection`1[Octokit.RepositoryContent]

--- Services/IEventManager.cs
+++ Services/IEventManager.cs
@@ -1,8 +1,31 @@
-﻿namespace PerformanceIssues.Serivces
+namespace PerformanceIssues.Services
 {
-    public interface IEventManager
+    public interface IEventManager : IDisposable
     {
         void Subscribe(Action<string> handler);
         void RaiseEvent(string message);
+    }
+    
+    public class EventManager : IEventManager
+    {
+        private readonly List<Action<string>> _handlers = new List<Action<string>>();
+
+        public void Subscribe(Action<string> handler)
+        {
+            _handlers.Add(handler);
+        }
+
+        public void RaiseEvent(string message)
+        {
+            foreach (var handler in _handlers)
+            {
+                handler.Invoke(message);
+            }
+        }
+
+        public void Dispose()
+        {
+            _handlers.Clear();
+        }
     }
 }
