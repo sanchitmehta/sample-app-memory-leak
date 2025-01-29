945a3db0844d03a23e5c88ca42636cf9cdc72a8d
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-29 00:28:48 -0800

    Fix memory leaks in System.Collections.ObjectModel.ReadOnlyCollection`1[Octokit.RepositoryContent]

--- Controllers/CPUController.cs
+++ Controllers/CPUController.cs
@@ -1,25 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using PerformanceIssues.Models;
-using PerformanceIssues.Serivces;
+using PerformanceIssues.Services;
+using System;
 
 namespace PerformanceIssuesDemo.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class CPUController : ControllerBase
+    public class CPUController : ControllerBase, IDisposable
     {
         private readonly CPUTaskManager _cpuTaskManager;
+        private bool _disposed;
 
         public CPUController(CPUTaskManager cpuTaskManager)
         {
-            _cpuTaskManager = cpuTaskManager;
+            _cpuTaskManager = cpuTaskManager ?? throw new ArgumentNullException(nameof(cpuTaskManager));
         }
 
         [HttpPost("start")]
         public IActionResult StartCPUTask([FromBody] CPUTaskRequest request)
         {
             if (request.Complexity <= 0 || request.Complexity > 1000000)
+            {
                 return BadRequest("Complexity must be between 1 and 1,000,000");
+            }
 
             var taskId = _cpuTaskManager.StartNewTask(request.Complexity);
             return Ok(new { taskId });
@@ -29,7 +33,9 @@ namespace PerformanceIssuesDemo.Controllers
         public IActionResult StopCPUTask(string taskId)
         {
             if (!_cpuTaskManager.StopTask(taskId))
+            {
                 return NotFound("Task not found");
+            }
 
             return Ok(new { message = "Task stopped successfully" });
         }
@@ -46,6 +52,25 @@ namespace PerformanceIssuesDemo.Controllers
         {
             _cpuTaskManager.StopAllTasks();
             return Ok(new { message = "All tasks stopped" });
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    // Dispose managed resources
+                    _cpuTaskManager?.Dispose();
+                }
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
