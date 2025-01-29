715baa6ba772c2c7f6689dc5379616d92d77d487
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-29 01:00:39 -0800

    Fix memory leaks in System.Collections.ObjectModel.ReadOnlyCollection`1[Octokit.RepositoryContent]

--- Services/CPUTaskManager.cs
+++ Services/CPUTaskManager.cs
@@ -1,13 +1,18 @@
-﻿using System.Collections.Concurrent;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 
-namespace PerformanceIssues.Serivces
+namespace PerformanceIssues.Services
 {
-    public class CPUTaskManager
+    public class CPUTaskManager : IDisposable
     {
         private readonly ConcurrentDictionary<string, ICPUIntensiveTask> _activeTasks = new();
+        private bool _disposed;
 
         public string StartNewTask(int complexity)
         {
+            if (_disposed) throw new ObjectDisposedException(nameof(CPUTaskManager));
+
             var taskId = Guid.NewGuid().ToString();
             var task = new CPUIntensiveTask(complexity);
             task.Start();
@@ -17,9 +22,15 @@ namespace PerformanceIssues.Serivces
 
         public bool StopTask(string taskId)
         {
+            if (_disposed) throw new ObjectDisposedException(nameof(CPUTaskManager));
+
             if (_activeTasks.TryRemove(taskId, out var task))
             {
                 task.Stop();
+                if (task is IDisposable disposableTask)
+                {
+                    disposableTask.Dispose();
+                }
                 return true;
             }
             return false;
@@ -27,16 +38,33 @@ namespace PerformanceIssues.Serivces
 
         public void StopAllTasks()
         {
-            foreach (var task in _activeTasks.Values)
+            if (_disposed) throw new ObjectDisposedException(nameof(CPUTaskManager));
+
+            foreach (var key in _activeTasks.Keys)
             {
-                task.Stop();
+                if (_activeTasks.TryRemove(key, out var task))
+                {
+                    task.Stop();
+                    if (task is IDisposable disposableTask)
+                    {
+                        disposableTask.Dispose();
+                    }
+                }
             }
             _activeTasks.Clear();
         }
 
         public IEnumerable<string> GetActiveTasks()
         {
+            if (_disposed) throw new ObjectDisposedException(nameof(CPUTaskManager));
             return _activeTasks.Keys;
+        }
+
+        public void Dispose()
+        {
+            if (_disposed) return;
+            StopAllTasks();
+            _disposed = true;
         }
     }
 }
