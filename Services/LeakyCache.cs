5e305c11ba8fb4cea997883d037ccc942b05875b
Sanchit Mehta <sanchit.mehta602@gmail.com>
2025-01-29 09:50:13 -0800

    Fix memory leaks in System.Collections.ObjectModel.ReadOnlyCollection`1[Octokit.RepositoryContent]

--- Services/LeakyCache.cs
+++ Services/LeakyCache.cs
@@ -1,25 +1,50 @@
-﻿using System.Collections.Concurrent;
+using System.Collections.Concurrent;
 
-namespace PerformanceIssues.Serivces
+namespace PerformanceIssues.Services
 {
-    public class LeakyCache : ILeakyCache
+    public class LeakyCache : ILeakyCache, IDisposable
     {
-        private static readonly ConcurrentDictionary<string, byte[]> _cache = new();
-        private static readonly Random _random = new();
+        private readonly ConcurrentDictionary<string, byte[]> _cache = new();
+        private readonly Random _random = new();
+        private bool _disposed = false;
 
         public async Task<string> AddToCache(string key, int sizeInMb)
         {
-            // Intentionally creating large byte arrays and storing them indefinitely
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(LeakyCache));
+
             byte[] data = new byte[sizeInMb * 1024 * 1024];
             _random.NextBytes(data);
 
-            // Simulate some async work
             await Task.Delay(100);
 
-            _cache.TryAdd(key, data);
+            _cache[key] = data;
             return key;
         }
 
-        public int GetCacheSize() => _cache.Count;
+        public int GetCacheSize()
+        {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(LeakyCache));
+                
+            return _cache.Count;
+        }
+
+        public void ClearCache()
+        {
+            foreach (var key in _cache.Keys)
+            {
+                _cache.TryRemove(key, out _);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                ClearCache();
+                _disposed = true;
+            }
+        }
     }
 }
